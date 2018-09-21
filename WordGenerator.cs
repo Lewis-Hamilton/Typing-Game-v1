50b7fcb0cc89d970e1593a14352bff43e1b0d15d
Lewis hamilton <38916472+Lewis-Hamilton@users.noreply.github.com>
2018-09-21 11:46:29 -0500

    Add files via upload

---
+++ WordGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour {
+
+	private static string[] wordList = {"This","is","the","best","game","ever","Jellyfish","NUT","Kosher","turquoise","gut","king","Butt","Discord","exuberance","hut","humongous","hotdog","phone"};
+
+	public static string GetRandomWord ()
+	{
+		int randomIndex = Random.Range(0, wordList.Length);
+		string randomWord=wordList[randomIndex];
+
+		return randomWord;
+	}
+
+}
